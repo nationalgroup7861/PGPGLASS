81b5a35808f388c52e680351a9d54d91fc1d37fd
aincoders <aincoders@gmail.com>
2024-01-18 13:11:02 +0530

    sdsss

---
+++ app/researchgpt/layout.js
@@ -0,0 +1,24 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
+
+export default function AuthLayout({ children }) {
+  const router = useRouter();
+  const { isAuth } = useSelector((state) => state.auth);
+  useEffect(() => {
+  
+    if (!isAuth) {
+      router.push("/");
+    }
+  }, [isAuth]);
+
+  return (
+    <>
+    
+        {children}
+    </>
+  );
+}
